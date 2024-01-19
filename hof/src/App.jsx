b718413b281eb1c531626d8346d149fa420dd7c1
Chumani-Lalasa <chumanilalasa@gmail.com>
2024-01-19 14:45:45 +0530

    rendering data

---
+++ hof/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HigherOrderFunctions from './Components/hof/HigherOrderFunctions'
+
+function App() {
+  return (
+    <div className="App">
+      <HigherOrderFunctions />
+    </div>
+  )
+}
+
+export default App
